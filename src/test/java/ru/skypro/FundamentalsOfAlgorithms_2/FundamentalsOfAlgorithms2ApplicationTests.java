622b88340e1114d4c97b758245bbda5b6387acac
Александр <ignatenko.ap@yandex.ru>
2024-03-04 15:54:32 +0300

    FundamentalsOfAlgorithms_2

---
+++ src/test/java/ru/skypro/FundamentalsOfAlgorithms_2/FundamentalsOfAlgorithms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.FundamentalsOfAlgorithms_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfAlgorithms2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
